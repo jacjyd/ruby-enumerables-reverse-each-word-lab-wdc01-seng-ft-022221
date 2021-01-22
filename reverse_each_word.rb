93a81df67730f11c76991289bfa02be889932d08
Diana Liao <dijali@gmail.com>
2021-01-22 22:04:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-  sentence.split.collect{ |word| word_reverse!}.join(" ")
+  sentence.split.collect{ |word| word.reverse!}.join(" ")
   
   
   #array = sentence.split
