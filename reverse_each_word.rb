38d8d1788825264331f591e9e803ba992a3b6ed9
Diana Liao <dijali@gmail.com>
2021-01-22 21:23:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require 'pry'
 def reverse_each_word(sentence)
   array = sentence.split
   array.collect do |word|
-    word = word.reverse
+    word.reverse
   end 
   rev_sentence = array.join(" ")
   rev_sentence
